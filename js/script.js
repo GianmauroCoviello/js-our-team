4b488e372a45eba4f7083ff06433146f61237135
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-05-31 18:47:19 +0200

    [DOCS] MILESTONE 0: Completata

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let persona1 ={
+    "nome": ['Wayne Barnett'],
+    "role": ['Founder & CEO'],
+    "immaggine": ['wayne-barner-founder-ceo.jpg ']
+}
+
+console.log(persona1)
+
+let persona2 ={
+    "nome": ['Angela Caroll'],
+    "role": ['Chief Editor'],
+    "immaggine": ['angela-caroll-chief-editor.jpg ']
+}
+
+console.log(persona2)
+
+let persona3 ={
+    "nome": ['Walter Gordon'],
+    "role": ['Office Manager'],
+    "immaggine": ['Walter-barner-office-manager.jpg ']
+}
+
+console.log(persona3)
+let persona4 ={
+    "nome": ['Angela Lopez'],
+    "role": ['Social Media Manager'],
+    "immaggine": ['angela-lopez-social-media-manager.jpg ']
+}
+
+console.log(persona4)
+
+let persona5 ={
+    "nome": ['Scott Estrada'],
+    "role": ['Develoaper'],
+    "immaggine": ['scott-estrada-develoaper.jpg ']
+}
+
+console.log(persona5)
+
+let persona6 ={
+    "nome": ['Barbara Ramos'],
+    "role": ['Graphic Designer'],
+    "immaggine": ['barbara-ramos-graphic-designer.jpg ']
+}
+
+console.log(persona6)
---
+++ readme.md
@@ -0,0 +1,12 @@
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite (file informazioni team).
+    SOLUZIONE- Inserire 6 objects con le informazioni che devono riportare i membri del team
+FINE
+
+MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
