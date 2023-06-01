c7eb55da41aaac00854e014fb4d54c9e2b5e055a
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-06-01 23:30:39 +0200

    [DOCS] MILESTONE 2: Completata. Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,77 +1,54 @@
 
-// PRIMO OBJECTS
-let membro_1 ={
-    "nome": 'Wayne Barnett',
-    "role": 'Founder & CEO',
-    "immaggine": 'wayne-barner-founder-ceo.jpg '
-}
-// KEY OBJECTS
-for (let key in membro_1) {
+// OBJECTS
+let fullTeam = [
+    {
+        'name': 'Wayne Barnett',
+        'role': 'Founder & CEO',
+        'image': 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'image': 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'image': 'walter-gordon-office-manager.jpg'
+    },
+    {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'image': 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'image': 'scott-estrada-developer.jpg'
+    },
+    {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'image': 'barbara-ramos-graphic-designer.jpg'
+    },
+]
 
-    console.log(membro_1[key])
-}
+// CICLO FOR PER STAMPARE LE CARD ALL'INTERNO DEL DOM
+for (let i = 0; i < fullTeam.length; i++) {
+    
+    console.log(fullTeam[i].name)
+    console.log(fullTeam[i].role)
+    console.log(fullTeam[i].image)
 
-
-
-let membro_2 ={
-    "nome": 'Angela Caroll',
-    "role": 'Chief Editor',
-    "immaggine": 'angela-caroll-chief-editor.jpg '
-}
-for (let key in membro_2) {
-
-    console.log(membro_2[key])
-}
-
-
-
-
-let membro_3 ={
-    "nome": 'Walter Gordon',
-    "role": 'Office Manager',
-    "immaggine": 'Walter-barner-office-manager.jpg '
-}
-for (let key in membro_3) {
-
-    console.log(membro_3[key])
-}
-
-
-
-let membro_4 ={
-    "nome": 'Angela Lopez',
-    "role": 'Social Media Manager',
-    "immaggine": 'angela-lopez-social-media-manager.jpg '
-}
-for (let key in membro_4) {
-
-    console.log(membro_4[key])
+    
 }
 
+// CICLO FOR PER INNIETTARE LE CARD ALL'INTERNO DEL DOM
+let compilate = document.querySelector('.card')
 
+for(let i = 0; i < fullTeam.length; i++){
+    compilate.innerHTML += `<h3>${fullTeam[i].name} </h3>`
+    compilate.innerHTML += `${fullTeam[i].role} <br>`
+    compilate.innerHTML += `${fullTeam[i].image}`
 
-
-let membro_5 ={
-    "nome": 'Scott Estrada',
-    "role": 'Develoaper',
-    "immaggine": 'scott-estrada-develoaper.jpg '
 }
-for (let key in membro_5) {
-
-    console.log(membro_5[key])
-}
-
-
-
-
-let membro_6 ={
-    "nome": 'Barbara Ramos',
-    "role": 'Graphic Designer',
-    "immaggine": 'barbara-ramos-graphic-designer.jpg '
-}
-for (let key in membro_6) {
-
-    console.log(membro_6[key])
-}
-
-
--- readme.md
+++ readme.md
@@ -2,13 +2,21 @@ Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membr
 
 MILESTONE 0:
 Creare l’array di oggetti con le informazioni fornite (file informazioni team).
-    SOLUZIONE- Inserire 6 objects con le informazioni che devono riportare i membri del team
+
+SOLUZIONE- Inserire 6 objects con le informazioni che devono riportare i membri del team
+
 FINE
 
 MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-    SOLUZIONE: stampiamo in console tutte le informazioni di ogni singolo membro del team tramite il ciclo for con al suo intero il console.log della key
+
+SOLUZIONE: stampiamo in console tutte le informazioni di ogni singolo membro del team tramite il ciclo for con al suo intero il console.log della key
+
 FINE
 
 
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+SOLUZIONE: inniettiamo tramite una variabile inserita al di fuori con la proprietà querySelector inserendo successivamente un ciclo for che cicla l'objects e innietta il tutto nel DOM
+
+FINE  
